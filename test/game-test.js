2aa7da1e35e5cdcfee81aae17339517179fef3da
Alex Pilewski <pilewski.alex@gmail.com>
2016-09-14 10:55:23 -0600

    test skeleton

--- test/game-test.js
+++ test/game-test.js
@@ -1,1 +1,9 @@
 const assert = require('chai').assert;
+
+const Lingo = require('../lib/game.js');
+
+describe('Lingo', function(){
+  it('should be an object', function(){
+    assert.isObject(Lingo);
+  });
+});
